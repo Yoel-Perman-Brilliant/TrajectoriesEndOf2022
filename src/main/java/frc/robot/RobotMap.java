3e4d72ed721d92d30d96df79ba504be9adaf6313
Yoel Perman Brilliant <87820740+Yoel-Perman-Brilliant@users.noreply.github.com>
2022-12-24 15:10:28 +0200

    Initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public interface CAN {
+
+    }
+    
+    public interface DIO {
+
+    }
+    
+    public interface PWM {
+
+    }
+    
+    public interface AIN {
+    
+    }
+
+    public interface PCM {
+
+    }
+}
